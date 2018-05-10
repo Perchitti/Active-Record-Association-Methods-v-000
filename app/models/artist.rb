7751bb5fb39301d73714b1de86b96155c3236eac
Lauren Perchitti <laurenperchitti@gmail.com>
2018-05-10 21:54:50 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,14 +3,14 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    self.songs.first
+    self.songs.first.genre
   end
 
   def song_count
-    self.song.count
+    self.songs.count
   end
 
   def genre_count
-    self.genre.count
+    self.genres.count
   end
 end
